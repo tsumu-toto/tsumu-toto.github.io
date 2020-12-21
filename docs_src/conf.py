cd1dcdf607d150fe60a1afcd1232fdb99c704632
tsumu-toto <akiyuky.s@icloud.com>
2020-12-21 13:49:37 +0900

    テーマを適用してみた

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -54,7 +54,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+from jupyter_sphinx_theme import *
+init_theme()
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
